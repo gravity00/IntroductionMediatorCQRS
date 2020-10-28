0321fc5ebc14a6b86945f0deffb0b146ec10f07a
João Simões <nogravity00@outlook.com>
2020-10-28 02:56:08 +0000

    Configured swagger and MVC

--- IntroductionMediatorCQRS/Startup.cs
+++ IntroductionMediatorCQRS/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,24 +9,27 @@ namespace IntroductionMediatorCQRS
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
 
+            services.AddSwaggerGen();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mediator ExampleApi V1");
+            });
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
