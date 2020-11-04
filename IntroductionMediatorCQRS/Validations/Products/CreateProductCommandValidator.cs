55651009e5483576a3c66e1c9fdd40243c89706b
João Simões <nogravity00@outlook.com>
2020-11-04 13:40:54 +0000

    Validators for product creation

---
+++ IntroductionMediatorCQRS/Validations/Products/CreateProductCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using IntroductionMediatorCQRS.Handlers.Products;
+
+namespace IntroductionMediatorCQRS.Validations.Products
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(e => e.Code)
+                .NotEmpty()
+                .Length(8)
+                .Matches("^[0-9a-zA-Z]*$");
+
+            RuleFor(e => e.Name)
+                .NotEmpty()
+                .MaximumLength(128);
+
+            RuleFor(e => e.Price)
+                .GreaterThanOrEqualTo(0);
+        }
+    }
+}
